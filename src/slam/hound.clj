638654f118a6774900649be77d41b32f173b8565
Phil Hagelberg <technomancy@gmail.com>
2011-04-23 22:53:04 -0700

    Basic import detection working.

--- src/slam/hound.clj
+++ src/slam/hound.clj
@@ -1,5 +1,8 @@
 (ns slam.hound
-  (:use [clojure.java.io :only [reader]]))
+  (:use [clojure.java.io :only [reader]]
+        [clojure.pprint :only [pprint]]
+        ;; TODO: extract
+        [swank.util.class-browse :only [available-classes]]))
 
 (def ns-clauses [:refer-clojure :use :require :import])
 
@@ -20,29 +23,53 @@
          (cons clause-type (clause-type ns-map)))))
 
 (defn missing-var [msg]
-  (symbol (second (re-seq #"Unable to resolve symbol: (\w+)" msg))))
+  (println :got msg)
+  ;; TODO: this doesn't catch require/as or static methods yet
+  (when-let [[match] (re-seq #"Unable to resolve \w+: (\w+)" msg)]
+    (second match)))
 
 (defn check-for-failure [ns-map body]
-  (try (eval `(do ~(ns-from-map ns-map) ~@body))
-       nil
+  (pprint ns-map)
+  (try (binding [*ns* (the-ns 'user)]
+         (eval (ns-from-map ns-map))
+         (doseq [form body] (eval form))
+         nil)
        (catch Exception e
          (missing-var (.getMessage e)))))
 
 (defn class-name? [x]
   (Character/isUpperCase (first (name x))))
 
-(defn add-import [failure ns-map])
-(defn add-require [failure ns-map])
-(defn add-use [failure ns-map])
+(defn qualified-class [class-name]
+  (some (fn [{full-name :name}]
+          (if (= (last (.split full-name "\\.")) class-name)
+            (symbol full-name)))
+        available-classes))
+
+(defn add-import [failure ns-map]
+  (let [class-name (qualified-class failure)]
+    ;; TODO: use style (:import (java.util UUID))
+    (update-in ns-map [:import] conj class-name)))
+
+(defn add-require [failure ns-map]
+  (println :require failure)
+  ns-map)
+
+(defn add-use [failure ns-map]
+  (println :use failure)
+  ns-map)
 
 (defn resolve-failure [failure ns-map]
   (cond (class-name? failure) (add-import failure ns-map)
         (namespace failure) (add-require failure ns-map)
         :else (add-use failure ns-map)))
 
-(defn reconstruct-ns-form [ns-map body]
+(defn reconstruct-ns-form [ns-map body last-failure]
   (if-let [failure (check-for-failure ns-map body)]
-    (recur (resolve-failure failure ns-map) body)))
+    (if (= failure last-failure)
+      (throw (Exception. (str "Couldn't resolve " failure)))
+      (recur (resolve-failure failure ns-map) body failure))
+    ns-map))
 
 (defn reconstruct [filename]
   (let [rdr (java.io.PushbackReader. (reader filename))
@@ -50,4 +77,4 @@
         stripped-ns (apply dissoc ns-map (rest ns-clauses))
         body (take-while #(not= ::done %)
                          (repeatedly #(read rdr false ::done)))]
-    (println (ns-from-map (reconstruct-ns-form stripped-ns body)))))
+    (pprint (ns-from-map (reconstruct-ns-form stripped-ns body nil)))))
--- test/slamhound/test_reconstruct.clj
+++ test/slamhound/test_reconstruct.clj
@@ -3,27 +3,41 @@
   (:use [clojure.test]
         [slam.hound]))
 
-(def sample-ns-form '(ns slamhound.test-reconstruct
+(def sample-ns-form '(ns slamhound.sample
                        "Testing some things going on here."
                        (:refer-clojure :exclude [compile test])
-                       (:use [clojure.test]
-                             [clojure.zip :only [zipper up]]
-                             [slam.hound])
+                       (:use [clojure.test :only [deftest is]]
+                             [slam.hound :only [ns-from-map]])
                        (:require [clojure.java.io :as io]
                                  [clojure.set :as set])
-                       (:import (java.io File ByteArrayInputStream)
+                       (:import (java.io File ByteArrayOutputStream)
                                 (java.util UUID))))
 
 (def sample-ns-map
-  {:name 'slamhound.test-reconstruct
+  {:name 'slamhound.sample
    :meta {:doc "Testing some things going on here."}
-   :use '([clojure.test] [clojure.zip :only [zipper up]] [slam.hound])
+   :use '([clojure.test :only [deftest is]] [slam.hound :only [ns-from-map]])
    :require '([clojure.java.io :as io] [clojure.set :as set])
-   :import '((java.io File ByteArrayInputStream) (java.util UUID))
+   :import '((java.io File ByteArrayOutputStream) (java.util UUID))
    :refer-clojure '(:exclude [compile test])})
+
+(def sample-body
+  '((set/union #{:a} #{:b})
+    (io/copy (File. "/tmp/remotely-human" (str (UUID/randomUUID)))
+             (ByteArrayOutputStream.))
+    (deftest test-ns-to-map
+      (is (= (ns-from-map {:ns 'slam.hound}))))))
 
 (deftest test-ns-to-map
   (is (= sample-ns-map (ns-to-map sample-ns-form))))
 
 (deftest test-ns-from-map
   (is (= sample-ns-form (ns-from-map sample-ns-map))))
+
+(deftest test-roundtrip
+  (is (= sample-ns-form (ns-from-map (ns-to-map sample-ns-form)))))
+
+(deftest test-add-import
+  (is (= sample-ns-map (reconstruct-ns-form
+                        (dissoc sample-ns-map :import)
+                        sample-body nil))))
