23e013db8eb96af6bb3594fc1a4fea3de831f8c7
Phil Hagelberg <technomancy@gmail.com>
2011-04-26 14:13:36 -0700

    1.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject slamhound "1.1.0"
+(defproject slamhound "1.1.1-SNAPSHOT"
   :description
   "Rips your ns form apart and reconstructs it. We have the technology."
   :dev-dependencies [[lein-difftest "1.3.1"]]
