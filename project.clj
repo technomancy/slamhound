356a6aab3f39fbe0942d90e12a14fdf51591f8be
Phil Hagelberg <technomancy@gmail.com>
2011-04-26 20:38:19 -0700

    Bump to 1.2.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject slamhound "1.1.1"
+(defproject slamhound "1.2.0-SNAPSHOT"
   :description
   "Rips your ns form apart and reconstructs it. We have the technology."
   :dev-dependencies [[lein-difftest "1.3.1"]]
