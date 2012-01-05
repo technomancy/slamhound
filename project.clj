6d5b17b32ffb24d431a22a486b691c3067d38c8c
Phil Hagelberg <technomancy@gmail.com>
2012-01-04 21:42:38 -0800

    Bump version number to 1.3.0-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,6 +1,4 @@
-(defproject slamhound "1.2.0"
+(defproject slamhound "1.3.0-SNAPSHOT"
   :description
   "Rips your ns form apart and reconstructs it. We have the technology."
-  :dev-dependencies [[lein-difftest "1.3.2"]]
-  :eval-in-leiningen true
-  :hooks [leiningen.hooks.difftest])
+  :eval-in-leiningen true)
