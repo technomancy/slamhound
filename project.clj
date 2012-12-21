2456a00ac7bc5d23157e2d662ee6db6d0e33cd57
Francis Wolke <francis@runa.com>
2012-12-21 15:43:47 -0800

    updated project.clj to use clojure 1.4 and swank

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject slamhound "1.3.0-SNAPSHOT"
-  :description
-  "Rips your ns form apart and reconstructs it. We have the technology.")
+  :description "Rips your ns form apart and reconstructs it. We have the technology."
+  :plugins [[lein-swank "1.4.4"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]])
