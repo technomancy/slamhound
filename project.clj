29b5cd5e569184eceef47a298582f92a40e85130
Francis Wolke <francis@runa.com>
2012-12-21 17:15:03 -0800

    added :unit test-selector to project file

--- project.clj
+++ project.clj
@@ -1,4 +1,7 @@
 (defproject slamhound "1.3.0-SNAPSHOT"
   :description "Rips your ns form apart and reconstructs it. We have the technology."
   :plugins [[lein-swank "1.4.4"]]
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :test-selectors {:default :unit
+                   :unit :unit})
+
