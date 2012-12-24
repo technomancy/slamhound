5d04fc5d527bce25b7987fbd54f09305968def41
Phil Hagelberg <technomancy@gmail.com>
2012-12-24 14:30:22 -0800

    Add license to project.clj.

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject slamhound "1.3.0-SNAPSHOT"
   :description "Rips your ns form apart and reconstructs it. We have the technology."
   :url "https://github.com/technomancy/slamhound"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.trace "0.7.3"]
                                   [org.clojars.runa/clj-schema "0.7.0"]
