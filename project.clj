1c02f2e33616b9798fc3085a04f286533403ca1e
Phil Hagelberg <technomancy@gmail.com>
2013-02-24 14:25:19 -0800

    Ensure we don't have AOT snuck in.
    This is from a bug in Leiningen 2 where :main still implies AOT on its own.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/technomancy/slamhound"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :main slam.hound
+  :main ^:skip-aot slam.hound
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.trace "0.7.3"]
                                   [org.clojars.runa/clj-schema "0.7.0"]
