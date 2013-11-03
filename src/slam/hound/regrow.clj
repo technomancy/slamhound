76bae3e0fba75fa8873e40965c714dffa715dc32
guns <self@sungpae.com>
2013-11-02 20:25:24 -0500

    Remove newly unused code in slam.hound.regrow
    New code and tests now supersede old forms at all points.

--- src/slam/hound/regrow.clj
+++ src/slam/hound/regrow.clj
@@ -60,17 +60,6 @@
                    :when alias]
                {(symbol alias) #{(symbol var-name)}})))))
 
-(defn- mass-refer-namespaces
-  "Extract the set of namespace symbols that match [_ :refer :all] from a
-  collection of libspecs."
-  [coll]
-  (reduce
-    (fn [s spec]
-      (if (and (coll? spec) (= (rest spec) [:refer :all]))
-        (conj s (first spec))
-        s))
-    #{} coll))
-
 (defn- ns-import-candidates
   "Search (all-ns) for imports that match missing-sym, returning a set of
   class symbols. This is slower than scanning through the list of static
@@ -129,28 +118,6 @@
       cs)
     candidates))
 
-(defn- expand-prefix-list [[prefix & more]]
-  (map (fn [expr]
-         (if (coll? expr)
-           (let [[x & xs] expr
-                 sym (symbol (str prefix \. x))]
-             (if (seq xs)
-               (into [sym] xs)
-               sym))
-           (symbol (str prefix \. expr))))
-       more))
-
-(defn expand-libs
-  "Reduce collection of symbols, libspecs, and prefix lists into a regular set
-  of symbols and libspecs"
-  [coll]
-  (reduce
-    (fn [s lib]
-      (if (and (coll? lib) (not (keyword? (second lib))))
-        (into s (expand-prefix-list lib))
-        (conj s lib)))
-    #{} coll))
-
 (defn- in-originals-pred [type missing old-ns-map]
   (fn [candidate]
     (case type
@@ -168,45 +135,12 @@
     @v
     #"swank|lancet"))
 
-(defn- new-type-to-old-types [new-type]
-  (case new-type
-    :require-as [:require]
-    :require-refer [:require :use] ; could've been require/refer or use/only
-    [new-type]))
-
-(defn- referred-to-in-originals-pred [type originals]
-  (if-not (= type :require-refer)
-    (constantly false)
-    (fn [[ns1 _ aliases1]]
-      (->> originals
-           (filter sequential?)
-           (filter #(contains? #{:only :refer} (second %)))
-           (some (fn [[ns2 _ aliases2]]
-                   (and (sequential? aliases1)
-                        (sequential? aliases2)
-                        (= (first aliases1) (first aliases2))
-                        (= ns1 ns2))))))))
-
 (defn- last-segment-matches-pred [type missing]
   (let [s (name missing)]
     (fn [candidate]
       (if (= type :alias)
         (= s (peek (string/split (name candidate) #"\.")))
         false))))
-
-(defn- disambiguate [candidates missing ns-map type]
-  ;; TODO: prefer things in src/classes to jars
-  (debug :disambiguating missing :in candidates)
-  (let [orig-clauses (mapcat #(get (:old ns-map) %)
-                             (new-type-to-old-types type))]
-    (->> candidates
-         (sort-by (juxt (complement (in-originals-pred orig-clauses))
-                        (complement (referred-to-in-originals-pred
-                                     type orig-clauses))
-                        (complement last-segment-matches?)
-                        (comp count str)))
-         (remove #(re-find disambiguator-blacklist (str %)))
-         first)))
 
 (defn disambiguate
   "Select the most likely class or ns symbol in the given set of candidates,
@@ -226,22 +160,6 @@
                (contains? (:refer-all old-ns-map) c))
         [:refer-all c]
         [type c]))))
-
-(defn- grow-step [missing type ns-map body]
-  (let [mass-refers (mass-refer-namespaces (get-in ns-map [:old :require]))
-        libspecs (candidates type missing body)
-        ;; Prefer original [_ :refer :all] specs
-        libspecs (reduce (fn [specs mref]
-                           (map (fn [spec]
-                                  (if (and (coll? spec)
-                                           (= (take 2 spec) [mref :refer]))
-                                    [mref :refer :all]
-                                    spec))
-                                specs))
-                         libspecs mass-refers)]
-    (if-let [addition (disambiguate libspecs missing ns-map type)]
-      (update-in ns-map [type] conj addition)
-      ns-map)))
 
 (defn grow-ns-map
   "Return a new ns-map augmented with a single candidate ns reference."
--- test/slam/hound/regrow_test.clj
+++ test/slam/hound/regrow_test.clj
@@ -2,10 +2,17 @@
   {:slamhound-skip true}
   (:require [clojure.test :refer [deftest is testing]]
             [korma.core]
-            [slam.hound.regrow :refer [candidates
-                                       disambiguate
-                                       grow-ns-map
-                                       regrow]]))
+            [slam.hound.regrow :refer [candidates disambiguate grow-ns-map]]))
+
+(def sample-body
+  '((set/union #{:a} #{:b})
+    (string/join ["a" "b"]) ; Try to conflict with set/join
+    (UUID/randomUUID)
+    (instance? Compiler$BodyExpr nil)
+    (io/copy (ByteArrayInputStream. (.getBytes "remotely human"))
+             (doto (File. "/tmp/remotely-human") .deleteOnExit))
+    (deftest ^:unit test-ns-to-map
+      (is (= (ns-from-map {:ns 'slam.hound}))))))
 
 ;; Classes and vars for testing
 (defrecord RegrowTestRecord [])
@@ -82,116 +89,3 @@
                         :refer 'pprint '((pprint [])))
            '{:old {:refer-all #{clojure.pprint}}
              :refer-all #{clojure.pprint}}))))
-
-(deftest ^:unit test-expand-libs
-  (testing "expands prefix lists into a flat list of symbols"
-    (is (= (expand-libs '((java.util Date Random UUID)))
-           '#{java.util.Date java.util.Random java.util.UUID})))
-  (testing "expands incomplete libspecs as symbols"
-    (is (= (expand-libs '[[clojure [set] [string]]])
-           '#{clojure.set clojure.string})))
-  (testing "returns regular libspecs as-is"
-    (is (= (expand-libs '[[clojure.set :as set] [clojure.string :as string]])
-           '#{[clojure.set :as set] [clojure.string :as string]})))
-  (testing "expands nested libspecs"
-    (is (= (expand-libs '[[clojure.java [io :as i] [shell :as s]]])
-           '#{[clojure.java.io :as i] [clojure.java.shell :as s]})))
-  (testing "everything at once"
-    (is (= (expand-libs '[java.util.regex.Pattern
-                          [java.util Date Random UUID]
-                          [clojure.pprint :as pp]
-                          [clojure [set] [string :as string]]
-                          [clojure.java [io :as io] [shell :refer [sh]]]])
-           '#{java.util.regex.Pattern
-              java.util.Date
-              java.util.Random
-              java.util.UUID
-              [clojure.pprint :as pp]
-              clojure.set
-              [clojure.string :as string]
-              [clojure.java.io :as io]
-              [clojure.java.shell :refer [sh]]}))))
-
-(def sample-body
-  '((set/union #{:a} #{:b})
-    (string/join ["a" "b"]) ; Try to conflict with set/join
-    (UUID/randomUUID)
-    (instance? Compiler$BodyExpr nil)
-    (io/copy (ByteArrayInputStream. (.getBytes "remotely human"))
-             (doto (File. "/tmp/remotely-human") .deleteOnExit))
-    (deftest ^:unit test-ns-to-map
-      (is (= (ns-from-map {:ns 'slam.hound}))))))
-
-(deftest ^:unit test-regrow
-  (testing "regrows the :require-as, :require-refer, and :imports onto ns-map"
-    (is (= {:name 'slamhound.sample
-            :meta {:doc "Testing some things going on here."}
-            :refer-clojure '(:exclude [compile test])
-            :gen-class nil
-            :require-as '[[clojure.java.io :as io]
-                          [clojure.string :as string]
-                          [clojure.set :as set]]
-            :require-refer '[[slam.hound.stitch :refer [ns-from-map]]
-                             [clojure.test :refer [is]]
-                             [clojure.test :refer [deftest]]]
-            :import '(java.io.File java.io.ByteArrayInputStream
-                                   clojure.lang.Compiler$BodyExpr
-                                   java.util.UUID)
-            :old {:name 'slamhound.sample
-                  :meta {:doc "Testing some things going on here."}
-                  :use '[[slam.hound.stitch :only [ns-from-map]]
-                         [clojure.test :only [is]]
-                         [clojure.test :only [deftest]]]
-                  :require '[[clojure.java.io :as io]
-                             [clojure.string :as string]
-                             [clojure.set :as set]]
-                  :import '[java.io.File java.io.ByteArrayInputStream
-                            clojure.lang.Compiler$BodyExpr
-                            java.util.UUID]
-                  :refer-clojure '(:exclude [compile test])
-                  :gen-class nil}}
-
-           (regrow [{:name 'slamhound.sample
-                     :meta {:doc "Testing some things going on here."}
-                     :refer-clojure '(:exclude [compile test])
-                     :gen-class nil
-                     :old {:name 'slamhound.sample
-                           :meta {:doc "Testing some things going on here."}
-                           :use '[[slam.hound.stitch :only [ns-from-map]]
-                                  [clojure.test :only [is]]
-                                  [clojure.test :only [deftest]]]
-                           :require '[[clojure.java.io :as io]
-                                      [clojure.string :as string]
-                                      [clojure.set :as set]]
-                           :import '[java.io.File
-                                     java.io.ByteArrayInputStream
-                                     clojure.lang.Compiler$BodyExpr
-                                     java.util.UUID]
-                           :refer-clojure '(:exclude [compile test])
-                           :gen-class nil}}
-                    sample-body]))))
-  (testing "prefers candidate where last segment matches"
-    (is (= (set (:require-as (regrow [{} sample-body])))
-           '#{[clojure.java.io :as io]
-              [clojure.string :as string]
-              [clojure.set :as set]})))
-  (testing "honors :refer :all in old ns"
-    (is (= (set (:require-refer
-                  (regrow [{:old {:require '[[clojure.test :refer :all]]}}
-                           sample-body])))
-           '#{[clojure.test :refer :all]
-              [slam.hound.stitch :refer [ns-from-map]]}))))
-
-(deftest ^:unit test-grow-preserve
-  (let [in-orig? (in-originals-pred '((java.util Date UUID)))]
-    (is (in-orig? 'java.util.Date))
-    (is (not (in-orig? 'java.sql.Date))))
-  (is (= '(java.io.File java.util.Date)
-         (:import (regrow [{:old {:import '((java.util Date))}}
-                           '[(Date.) (File. "/tmp")]])))))
-
-(def +i-must-be-a-cl-user+ true)
-
-(deftest ^:unit test-plus
-  (is (= '[[slam.hound.regrow-test :refer [+i-must-be-a-cl-user+]]]
-         (:require-refer (regrow [{} '[+i-must-be-a-cl-user+]])))))
